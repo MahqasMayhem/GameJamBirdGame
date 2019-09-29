fec76bda59649d57ec5662eb738a549cfa330a3b
Dakota <dakotajensen04@gmail.com>
2019-09-28 22:22:09 -0500

    Player Prefab and several code adjustments

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,10 +21,14 @@ class WalkScript : MonoBehaviour
     public bool rested = true;
     private int reps = 0;
     private float xAngle, yAngle, zAngle;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
+        MissionComplete = GameObject.Find("UI_MissionWin");
+        MissionComplete.SetActive(false);
+        anim = transform.Find("Pigeon").gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,18 +37,50 @@ class WalkScript : MonoBehaviour
         if (Input.GetKeyDown("space"))
         {
             fly = !fly;
+            if (!fly)
+                anim.SetBool(0, true);
+            else
+                anim.SetBool(0, false);
         }
 
         if (fly)
         {
             Fly();
         }
+
         else
         {
             Walk();
         }
+        if (!fly && transform.rotation.x != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, transform.rotation.y, 0f);
+        }
     }
 
+
+    private bool CanRotate(bool IsRoll, bool positive)
+    {
+        Transform comparisonT = GameObject.Find("BirdCompare").GetComponent<Transform>();
+       // float valueX = comparisonT.rotation.eulerAngles.x - transform.rotation.eulerAngles.x;
+        float valueZ = Vector3.Angle(transform.right, Vector3.right);
+        //float valueZ = comparisonT.rotation.eulerAngles.z - transform.rotation.eulerAngles.z;
+        float valueX = Vector3.Angle(transform.forward, Vector3.forward);
+        Debug.Log("X:" + valueX);
+        Debug.Log("Z:" + valueZ);
+        if (IsRoll)
+        {
+            if (valueZ > -65 && positive) return true;
+            else if (valueZ < 65 && !positive) return true;
+            else return false;
+        }
+        else
+        {
+            if (valueX > -65 && positive) return true;
+            else if (valueX < 65 && !positive) return true;
+            else return false;
+        }
+    }
     void Fly()
     {
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
@@ -56,31 +93,33 @@ class WalkScript : MonoBehaviour
             cc.Move(transform.forward * (zVo) * Time.deltaTime);
             //rb.velocity = transform.forward * zVo; 
         }
-
-        if (Input.GetKey("s") || Input.GetKey("down"))
+        /*
+        if ((Input.GetKey("s") || Input.GetKey("down")) && CanRotate(false))
         {
+
             //transform.Rotate(1f, 0, 0); 
             transform.Rotate(Vector3.right, 1f);
         }
 
-        if (Input.GetKey("up") || Input.GetKey("w"))
+        if ((Input.GetKey("up") || Input.GetKey("w")) && CanRotate(false))
         {
+            Debug.Log("Rotate Down");
             //transform.Rotate(-1f, 0, 0); 
             transform.Rotate(Vector3.right, -1f);
         }
 
-        if (Input.GetKey("a") || Input.GetKey("left"))
+        if ((Input.GetKey("a") || Input.GetKey("left")) && CanRotate(true))
         {
             //transform.Rotate(0, -1f, 0); 
             transform.Rotate(Vector3.up, -1f);
         }
 
-        if (Input.GetKey("d") || Input.GetKey("right"))
+        if ((Input.GetKey("d") || Input.GetKey("right")) && CanRotate(true))
         {
             //transform.Rotate(0, 1f, 0); 
             transform.Rotate(Vector3.up, 1f);
         }
-
+        */
 
         if (reps > maxReps)
         {
@@ -91,10 +130,45 @@ class WalkScript : MonoBehaviour
 
         if (readyToFly && rested)
         {
-            readyToFly = false;
-            //StartCoroutine("FlapWing"); 
+            readyToFly = false; 
+        }
+        Quaternion targetRotation = transform.rotation;
+        Vector3 targetAngles = targetRotation.eulerAngles;
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+
+            //transform.Rotate(1f, 0, 0); 
+            targetAngles.x += 1f;
+            //transform.Rotate(Vector3.right, 1f);
         }
 
+        if (Input.GetKey("up") || Input.GetKey("w"))
+        {
+            Debug.Log("Rotate Down");
+            //transform.Rotate(-1f, 0, 0); 
+            targetAngles.x -= 1f;
+            //transform.Rotate(Vector3.right, -1f);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            //transform.Rotate(0, -1f, 0); 
+            targetAngles.y -= 1f;
+            //transform.Rotate(Vector3.up, -1f);
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            //transform.Rotate(0, 1f, 0); 
+            targetAngles.y += 1f;
+            //transform.Rotate(Vector3.up, 1f);
+        }
+        Debug.Log(targetAngles);
+        targetAngles.x = ClampAngle(targetAngles.x, -65, 65);
+        targetAngles.z = ClampAngle(targetAngles.z, -65, 65);
+        targetRotation = Quaternion.Euler(targetAngles);
+        transform.rotation = targetRotation;
+        /*
         float spinangle = BirdClamp(transform.rotation.eulerAngles.y, minAngle, maxAngle);
         Debug.Log(spinangle + " " + transform.rotation.eulerAngles.y);
 
@@ -106,9 +180,51 @@ class WalkScript : MonoBehaviour
                 return 315;
             return cur;
         }
+        */
+    
 
+}
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        angle = Mathf.Repeat(angle, 360);
+        min = Mathf.Repeat(min, 360);
+        max = Mathf.Repeat(max, 360);
+        bool inverse = false;
+        var tmin = min;
+        var tangle = angle;
+        if (min > 180)
+        {
+            inverse = !inverse;
+            tmin -= 180;
+        }
+        if (angle > 180)
+        {
+            inverse = !inverse;
+            tangle -= 180;
+        }
+        var result = !inverse ? tangle > tmin : tangle < tmin;
+        if (!result)
+            angle = min;
+
+        inverse = false;
+        tangle = angle;
+        var tmax = max;
+        if (angle > 180)
+        {
+            inverse = !inverse;
+            tangle -= 180;
+        }
+        if (max > 180)
+        {
+            inverse = !inverse;
+            tmax -= 180;
+        }
+
+        result = !inverse ? tangle < tmax : tangle > tmax;
+        if (!result)
+            angle = max;
+        return angle;
     }
-
 
 
     private void Walk()
@@ -133,6 +249,11 @@ class WalkScript : MonoBehaviour
         {
             //rb.velocity = transform.right * speed;
             transform.Rotate(0, 1.5f, 0);
+        }
+        if (!cc.isGrounded)
+        {
+            var gravity = Physics.gravity * Time.deltaTime;
+            cc.Move(transform.up * gravity.y);
         }
     }
 }
